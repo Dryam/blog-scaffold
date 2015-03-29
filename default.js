c72b911f4915df4c0a280fb39c7f674fb94fe35a
Dryam <dryam.zen@gmail.com>
2015-03-29 05:31:56 +0300

    added out for artile path categorys and subCategorys

--- default.js
+++ default.js
@@ -1,57 +1,62 @@
         document.addEventListener('polymer-ready', function() {
-            
-            var template = document.querySelector('#page-template');
-            var ajax = document.querySelector("core-ajax");
-            
-            //download the articles-list             
-            ajax.url = 'articles/articles.json';
+                
+                var template = document.querySelector('#page-template');
+                var ajax = document.querySelector("core-ajax");                
+                ajax.url = 'articles/articles.json';                                
+                
+                var bb = document.querySelector('#button1');
+                        console.log(bb);
+                                                                                                                       
+                ajax.addEventListener("core-response", function(){                        
+                    document.querySelector("core-list").data = ajax.response;                                        
+                })
+                
+                ajax.addEventListener("core-response",
+                        //when articles-list downloaded start generate categorys list
+                        function() {
+                                                                                        
+                            var menuCat = [];
+                            var menu = [];
+                            
 
-            //when articles-list downloaded start generate categorys list
-            ajax.addEventListener("core-response",
-                function() {
-                    var menuCat = [];
-                    var menu = [];
+                            //Making from articles-list.json object with unique categorys and unique subCategorys                     
+                            ajax.response.forEach(getCategorys);
 
-                    //Making from articles-list.json object with unique categorys and unique subCategorys                     
+                            //Making menu-array from object with unique categorys and unique subCategorys
+                            makeMenu();
 
-                    ajax.response.forEach(getCategorys);
-                    
-                    //Making menu-array from object with unique categorys and unique subCategorys
-                    
-                    makeMenu();
-                    
-                    //put unique fields of articles-list.json to categorys object.                    
-                    template.categorys = menu;
+                            //put unique fields of articles-list.json to categorys object.                    
+                            template.categorys = menu;
 
-                    function getCategorys(item) {                        
-                        for (var i = 0; i < item.categorys.length; i++) {
-                            if (menuCat[item.categorys[i]] == undefined) {
-                                menuCat[item.categorys[i]] = {}
+                            function getCategorys(item) {
+                                for (var i = 0; i < item.categorys.length; i++) {
+                                    if (menuCat[item.categorys[i]] == undefined) {
+                                        menuCat[item.categorys[i]] = {}
 
-                                for (var j = 0; j < item.subCategorys.length; j++) {
-                                    menuCat[item.categorys[i]][item.subCategorys[j]] = {};
+                                        for (var j = 0; j < item.subCategorys.length; j++) {
+                                            menuCat[item.categorys[i]][item.subCategorys[j]] = {};
+                                        }
+                                    } else {
+                                        for (var s = 0; s < item.subCategorys.length; s++) {
+                                            menuCat[item.categorys[i]][item.subCategorys[s]] = {};
+                                        }
+                                    }
                                 }
-                            } else {
-                                for (var s = 0; s < item.subCategorys.length; s++) {
-                                    menuCat[item.categorys[i]][item.subCategorys[s]] = {};
+
+                            }
+
+                            function makeMenu() {
+                                for (var i = 0; i < Object.keys(menuCat).length; i++) {
+                                    menu.push({
+                                        'name': Object.keys(menuCat)[i],
+                                        'subcategorys': Object.keys(menuCat[Object.keys(menuCat)[i]])
+                                    });
                                 }
                             }
                         }
+                    );
 
-                    }
-
-                    function makeMenu() {
-                        for (var i = 0; i < Object.keys(menuCat).length; i++) {
-                            menu.push({
-                                'name': Object.keys(menuCat)[i],
-                                'subcategorys': Object.keys(menuCat[Object.keys(menuCat)[i]])
-                            });
-                        }
-                    }                                                                                         
-                }
-            );
-        }
+            }
 
         );
-// TODO: try to rewrite in module with facade.        
-    
+        // TODO: try to rewrite in module with facade.
